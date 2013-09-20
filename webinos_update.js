d6a3d536aeb55683199c30c65dc4dceb6da66e60
Vito Macchia <vito.macchia@gmail.com>
2013-09-20 11:21:33 +0200

    refined search in dependency folders

--- webinos_update.js
+++ webinos_update.js
@@ -4,61 +4,99 @@ var childProcess = require('child_process');
 
 var prompt = require("prompt");
 
-var WEBINOS_DIR = 'WEBINOS';
-var webinosMainDirectory = path.resolve(process.env.HOME, WEBINOS_DIR);
-
-
+var WEBINOS_PROJ_DIR = 'WEBINOS'; //all webinos-* and hub-* projects must be under this directory
+var webinosMainDirectory = path.resolve(process.env.HOME, WEBINOS_PROJ_DIR);
 process.chdir(webinosMainDirectory);
 
-function updateProject(projectDirectory, dirname, index) {
-  var isWebinos = dirname.match(/webinos-/g) || dirname.match(/hub-/g);
-  //console.log(index + ") Dir:", projectDirectory.bold.blue, isWebinos ? ("YES").bold.green : ("NO").bold.red);
-  if (isWebinos) {
-    var updateProc = childProcess.spawn('git', ['pull'], {
-      cwd: projectDirectory
-    });
-    updateProc.on('error', function(err) {
-      console.log(("Error updating webinos project: " + dirname).red, err);
-    });
-    var childOutput = '';
-    updateProc.stdout.on('data', function(chunk) {
-      if (chunk) {
-        childOutput += chunk;
-      }
-    });
-    updateProc.stdout.on('end', function() {
-      if (childOutput.length > 0)
-      {
-        if (childOutput.replace(/\r?\n|\r/g, '') === "Already up-to-date.")
-          console.log(dirname.yellow + " status:", ("up to date").green);
-      }
-    });
+var MAX_DEPTH = 2; //max depth of webinos dependency
+var counter = 0;
+var updated = [];
 
-
-//    updateProc.on('close', function() {
-//      console.log("webinos project: " + dirname + (" exited correctly").bold.green + "\n");
-//    });
-  }
-}
-
-(function() {
-
-  fs.readdir(webinosMainDirectory, function(err, files) {
-    if (err) {
-      console.error("Cannot read webinos main directory", err.red);
+//call git pull for each project directory
+function updateWebinosProject(projectDirectory, dirname, level) {
+  console.log("Checking", projectDirectory, "for updates...");
+  var updateProc = childProcess.spawn('git', ['pull'], {
+    cwd: projectDirectory
+  });
+  updateProc.on('error', function(err) {
+    console.log(("Error updating webinos project: " + dirname).red, err);
+  });
+  var childOutput = '';
+  updateProc.stdout.on('data', function(chunk) {
+    if (chunk) {
+      childOutput += chunk;
     }
-    else {
-      var k = 0;
-      for (var i = 0; i < files.length; i++) {
-        (function(i, file) {
-          var filePath = path.resolve(file);
-          fs.stat(filePath, function(err, stats) {
-            if (!err && stats.isDirectory()) {
-              updateProject(filePath, file, k++);
-            }
-          });
-        })(i, files[i]);
+  });
+  updateProc.stdout.on('end', function() {
+    if (childOutput.length > 0)
+    {
+      if (childOutput.replace(/\r?\n|\r/g, '') === "Already up-to-date.") {
+        console.log(dirname.yellow + " status:", ("up to date").green);
+      }
+      else {
+        var match = childOutput.match(/remote: Counting objects: (\d)+, done./);
+        if (match && match.index === 0) {
+          updated.push(projectDirectory);
+        }
       }
     }
   });
-})();
+//    updateProc.on('close', function() {
+//      console.log("update process on: " + dirname + (" exited correctly").bold.green + "\n");
+//    });
+
+  //update API and other stuff in subfolders
+  var node_modules = path.join(projectDirectory, 'node_modules');
+  fs.exists(node_modules, function(exists) {
+    if (exists) {
+      console.log(" Entering level", level, ":", node_modules.magenta);
+      process.chdir(node_modules);
+      scanDirectory(node_modules, level + 1);
+    }
+  });
+}
+
+//select webinos-* and hub-* directories, then update them
+function searchForWebinosDirectories(file, level) {
+  var filePath = path.resolve(file);
+  fs.stat(filePath, function(err, stats) {
+    if (!err && stats.isDirectory()) {
+
+      var isWebinos = file.match(/webinos-/g) || file.match(/hub-/g);
+      //    console.log(filePath.bold.blue, "is a webinos project?", isWebinos ? ("YES").bold.green : ("NO").bold.red);
+      if (isWebinos) {
+        counter++;
+        updateWebinosProject(filePath, file, level);
+      }
+    }
+  });
+}
+
+//scan directory dirname
+//starting at level level
+function scanDirectory(dirname, level) {
+  if (level <= MAX_DEPTH) {
+    fs.readdir(dirname, function(err, files) {
+      if (err) {
+        console.error("Cannot read " + dirname + ":", err.red);
+      }
+      else {
+        for (var i = 0; i < files.length; i++) {
+          searchForWebinosDirectories(files[i], level + 1);
+        }
+      }
+    });
+  }
+}
+
+//Go!
+scanDirectory(webinosMainDirectory, 0);
+
+
+process.on('exit', function() {
+  console.log(("\n******************************************************".rainbow.bold));
+  console.log("Exiting. Found", counter, "webinos folders, ", updated.length, " updated.");
+  updated.forEach(function(folderName) {
+    console.log(folderName.yellow.bold, "was updated.", ("Please run any required action in its folder (grunt, gyp-build, npm-install, whatever...)").underline);
+  });
+});
